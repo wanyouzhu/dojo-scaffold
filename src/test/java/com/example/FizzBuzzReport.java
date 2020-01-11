f1d3f6345757ef051e456550a943f8e57735fb72
朱万友 <van@highsoft.ltd>
2020-01-11 15:44:16 +0800

    Refactored 'check total lines'.

--- src/test/java/com/example/FizzBuzzReport.java
+++ src/test/java/com/example/FizzBuzzReport.java
@@ -5,6 +5,10 @@ import java.util.stream.IntStream;
 
 public class FizzBuzzReport {
     public String report() {
-        return IntStream.range(1, 101).mapToObj(x -> String.valueOf(x)).collect(Collectors.joining("\n"));
+        return IntStream.range(1, 101).mapToObj(this::lineForTurn).collect(Collectors.joining("\n"));
+    }
+
+    private String lineForTurn(int turn) {
+        return String.valueOf(turn);
     }
 }
